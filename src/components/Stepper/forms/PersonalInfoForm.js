ffa3dfc7a7f2af9098465832bb49b54405e39a5a
Albados <devopsnhc39@gmail.com>
2023-03-02 10:45:17 +0530

    sss
    sss

--- src/components/Stepper/forms/PersonalInfoForm.js
+++ src/components/Stepper/forms/PersonalInfoForm.js
@@ -15,13 +15,13 @@ const PersonalInfoForm = ({ handleNext }) => {
       validationSchema={Yup.object({
         //
         // Validate FirstName
-        firstName: Yup.string().firstName("Invalid name")
+        firstName: Yup.string().max("Invalid name")
 		.required("required"),
           
 		  
 
         // Validate LastName
-        lastName: Yup.string().lastName(20, "Must be 20 character or less")
+        lastName: Yup.string().max(20, "Must be 20 character or less")
 		  .required("required"),
 		  
           
