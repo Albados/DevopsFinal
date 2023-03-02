ca226b9c0447abdd09476706a86e5bf6a9422657
Albados <devopsnhc39@gmail.com>
2023-03-02 09:32:22 +0530

    changes in home page
    spelling mistake

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+*+ vimport React from "react";
 import Shoes from "../../latestShoes.json";
 
 function Home() {
@@ -6,9 +6,9 @@ function Home() {
     <div>
       <div className="banner-image"></div>
       <div>
-        <span className="tagline">JUST DOES IT</span>
+        <span className="tagline">JUST DO IT</span>
         <div className="tagline-para">
-          Nike has been your compadsdvsdvnion in happiness, both big and small, for
+          Nike has been your companion in happiness, both big and small, for
           over 50 years. Our roots are firmly grounded in India and with its
           people.
         </div>
